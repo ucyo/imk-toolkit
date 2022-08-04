b2165fbc15344a9ef89ba00bf35e51a10542565b
ucyo <cayoglu@me.com>
2022-08-04 10:55:22 +0200

    First serve of the routine

--- imktk/dataarray_methods/satellite_tracks.py
+++ imktk/dataarray_methods/satellite_tracks.py
@@ -8,24 +8,32 @@ This script helps calculating interpolations along satellite tracks.
 import xarray as xr
 
 
-def main(dataarray):
+def main(dataarray, lat, lon, name=None):
     """Interpolation routine to satellite tracks
 
     This script helps calculating interpolations along satellite tracks.
 
     Arguments
     =========
-    dataarray: <Description>
-    argument1: <Description>
-    ...
-    argumentX: <Description>
+    dataarray: xr.DataArray to be mapped on the Satellite tracks
+    lat: List of latitude values
+    lon: List of longitude values
+    name: Name of satellite
 
     Returns
     =======
-    dataarray: <Description>
+    result: xr.DataArray of interpolated values to satellite track
     """
     assert isinstance(dataarray, xr.DataArray), "Input is not an xr.DataArray"
+    assert isinstance(lat, list), "Latitude is not a list"
+    assert isinstance(lon, list), "Longitude is not a list"
+    assert len(lon) == len(lat), f"Longitude and Latitute lists are not of same length {len(lon)} != {len(lat)}"
+    assert "lat" in dataarray.data_coords, f"Lat(itute) not found in {dataarray.data_coords}"
+    assert "lon" in dataarray.data_coords, f"Lon(gitude) not found in {dataarray.data_coords}"
 
-    # add data processing code here
+    if not name:
+        name = "satellite"
+    lon = xr.DataArray(lon, dims=name)
+    lat = xr.DataArray(lat, dims=name)
 
-    return dataarray
+    return dataarray.sel(lon=lon, lat=lat)
